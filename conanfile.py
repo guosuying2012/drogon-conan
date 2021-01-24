5cf92c67971bb4a7f4dc0127fe766330a3f40b59
yengsu <yengsu@yengsu.com>
2021-01-24 12:14:08 +0800

    完善conanfile设置，编写测试程序

--- conanfile.py
+++ conanfile.py
@@ -4,48 +4,51 @@ from conans import ConanFile, CMake, tools
 class DrogonConan(ConanFile):
     name = "drogon"
     version = "1.3.0"
-    license = "<Put the package license here>"
-    author = "<Put your name here> <And your email here>"
-    url = "<Package recipe repository url here, for issues about the package>"
-    description = "<Description of Drogon here>"
-    topics = ("<Put some tag here>", "<here>", "<and here>")
+    license = "MIT"
+    author = "an-tao"
+    url = "https://github.com/an-tao/drogon"
+    description = "Drogon: A C++14/17 based HTTP web application framework running on Linux/macOS/Unix/Windows "
+    topics = ("linux", " http", "http-server", "asynchronous-programming", "non-blocking-io", "drogon", "http-framework")
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False], "fPIC": [True, False]}
     default_options = {"shared": False, "fPIC": True}
-    generators = "cmake"
+    generators = "cmake", "cmake_find_package", "cmake_paths"
+    requires = ["trantor/1.3.0", "jsoncpp/1.9.4", "libuuid/1.0.3", "zlib/1.2.11", "openssl/1.1.1i", "c-ares/1.17.1", "brotli/1.0.9", "libpq/13.1", "mariadb-connector-c/3.1.11", "sqlite3/3.34.0"]
 
     def config_options(self):
         if self.settings.os == "Windows":
             del self.options.fPIC
 
     def source(self):
-        self.run("git clone https://github.com/conan-io/hello.git")
+        self.run("git clone https://github.com/an-tao/drogon.git")
         # This small hack might be useful to guarantee proper /MT /MD linkage
         # in MSVC if the packaged project doesn't have variables to set it
         # properly
-        tools.replace_in_file("hello/CMakeLists.txt", "PROJECT(HelloWorld)",
-                              '''PROJECT(HelloWorld)
+        tools.replace_in_file("drogon/CMakeLists.txt", "project(drogon)",
+                              '''project(drogon)
 include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
-conan_basic_setup()''')
+conan_basic_setup()
+include(${CMAKE_BINARY_DIR}/conan_paths.cmake)
+set(CMAKE_MODULE_PATH ${CMAKE_BINARY_DIR} ${CMAKE_MODULE_PATH})''')
 
     def build(self):
         cmake = CMake(self)
-        cmake.configure(source_folder="hello")
+        cmake.configure(source_folder="drogon")
         cmake.build()
 
         # Explicit way:
-        # self.run('cmake %s/hello %s'
+        # self.run('cmake %s/drogon %s'
         #          % (self.source_folder, cmake.command_line))
         # self.run("cmake --build . %s" % cmake.build_config)
 
     def package(self):
-        self.copy("*.h", dst="include", src="hello")
-        self.copy("*hello.lib", dst="lib", keep_path=False)
+        self.copy("*.h", dst="include", src="drogon")
+        self.copy("*drogon.lib", dst="lib", keep_path=False)
         self.copy("*.dll", dst="bin", keep_path=False)
         self.copy("*.so", dst="lib", keep_path=False)
         self.copy("*.dylib", dst="lib", keep_path=False)
         self.copy("*.a", dst="lib", keep_path=False)
 
     def package_info(self):
-        self.cpp_info.libs = ["hello"]
+        self.cpp_info.libs = ["drogon"]
 
--- test_package/example.cpp
+++ test_package/example.cpp
@@ -1,5 +1,11 @@
-#include "drogon.h"
+#include <iostream>
+#include <drogon/utils/Utilities.h>
 
-int main() {
-    drogon();
+using namespace std;
+using namespace drogon;
+
+int main(int atgc, char* argv[]) 
+{
+    cout << "RandomString: " << utils::genRandomString(10) <<endl;
+    return 0;
 }
